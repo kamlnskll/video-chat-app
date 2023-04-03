10d8ece8da1710aa99d93bfff5835332caf6405e
Michael Kaminski <michael_kaminski@outlook.com>
2023-04-03 14:16:30 -0500

    more work on user routes

--- client/src/axios/userRoutes.ts
+++ client/src/axios/userRoutes.ts
@@ -1,9 +1,30 @@
 import axios from 'axios'
 
-export const registerNewUser = () => {
+export const registerNewUser = async (firstName: string, lastName: string, userName: string, email: string, password: string) => {
+const data = {
+    firstName: firstName,
+    lastName: lastName,
+    userName: userName,
+    email: email,
+    password: password
+}
+
+await axios.post(`http://localhost:3000/api/user/register`, { data }).then((res) => {
+    return res.data
+})
 
 }
 
-export const loginUser = () => {
-    
+export const loginUser = async (userName: string, password: string) => {
+    const data = {
+        userName: userName,
+        password: password
+    }
+
+    await axios.post(`http://localhost:3000/api/user/login`, { data }).then((res) => {
+        return res.data
+    })
+
+
+
 }
