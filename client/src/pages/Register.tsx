80fbb8202af2cfa8b082d8aeec669fa92d328861
Michael Kaminski <michael_kaminski@outlook.com>
2023-04-03 14:01:40 -0500

    added axios

--- client/src/pages/Register.tsx
+++ client/src/pages/Register.tsx
@@ -1,7 +1,30 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const Register = () => {
-  return <div>Register</div>
+
+const [firstName, setFirstName] = useState('')
+const [lastName, setLastName] = useState('')
+const [email, setEmail] = useState('')
+const [userName, setUserName] = useState('')
+const [password, setPassword] = useState('')
+
+  return (
+    <div className='w-full h-screen'>
+      <div className='border bg-gray-50 w-1/2 mx-auto h-1/2'>
+      <div className='pt-8 pb-4'>
+      <input name="firstName" placeholder="First name" type="text" className='flex mx-auto w-3/4 h-[50px] outline-none text-sm pl-6 mb-4' onChange={(e) => setFirstName(e.target.value)} />
+      <input name="username" placeholder="Last name" type="text" className='flex mx-auto w-3/4 h-[50px] outline-none text-sm pl-6 mb-4' onChange={(e) => setLastName(e.target.value)} />
+      <input name="email" placeholder="Email" type="text" className='flex mx-auto w-3/4 h-[50px] outline-none text-sm pl-6 mb-4' onChange={(e) => setEmail(e.target.value)} />
+      <input name="username" placeholder="Username" type="text" className='flex mx-auto w-3/4 h-[50px] outline-none text-sm pl-6 mb-4' onChange={(e) => setUserName(e.target.value)} />
+      <input name="password" placeholder="Password" type="password" className='flex mx-auto w-3/4 h-[50px] outline-none text-sm pl-6' onChange={(e) => setPassword(e.target.value)}/>
+      </div>
+      <button type='button' className='font-bold py-1 bg-blue-300 mx-auto w-3/4' onClick={() => console.log(userName, password)}>
+        Register
+      </button>
+      </div>
+      
+    </div>
+  )
 }
 
 export default Register
