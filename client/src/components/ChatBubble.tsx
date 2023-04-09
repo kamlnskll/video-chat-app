8932a29a60013cb0c8cb1f22cf0702134aac8c4c
Michael <michael_kaminski@outlook.com>
2023-04-09 17:19:08 -0500

    implemented basic style for sender chat bubbles

--- client/src/components/ChatBubble.tsx
+++ client/src/components/ChatBubble.tsx
@@ -9,9 +9,12 @@ const ChatBubble = ({ chat, isMe }: ChatProps) => {
   return (
     <>
       {isMe ? (
-        <div>
-          <h1>Michael</h1>
-          <h1>{chat}</h1>
+        <div className='bg-blue-400 mt-4 mx-4 rounded-xl px-2 py-2 text-white'>
+          <div className='flex'>
+            <h1 className='text-xs font-semibold'>Michael Kaminski</h1>
+            <h1 className='text-xs ml-12'>Timestamp</h1>
+          </div>
+          <h1 className=''>{chat}</h1>
         </div>
       ) : (
         <div>
