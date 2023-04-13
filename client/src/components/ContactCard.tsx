02ca5e78cf699c95be660db3c340289f58b07322
Michael <michael_kaminski@outlook.com>
2023-04-13 14:44:35 -0500

    getting started on contact page

---
+++ client/src/components/ContactCard.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ContactCard = () => {
+  return (
+    <div>
+      <h1>This will be the contact card with full info</h1>
+    </div>
+  )
+}
+
+export default ContactCard
--- client/src/pages/Contacts.tsx
+++ client/src/pages/Contacts.tsx
@@ -8,7 +8,18 @@ const Contacts = () => {
       <div>
         <Navbar />
       </div>
-      <div className='mx-auto border bg-white w-[500px] h-[500px]'></div>
+      <div className='mx-auto border bg-white w-[500px] h-[500px] mt-12 relative'>
+        <h1 className='text-center font-semibold text-xl mt-4'>Contacts (0)</h1>
+        <button
+          type='button'
+          className='border px-2 text-sm text-white rounded-lg font-semibold bg-orange-600 absolute top-5 right-4'
+        >
+          Add Contact
+        </button>
+        <div className='mt-10'>
+          <h1>Contact Cards Mapped Here</h1>
+        </div>
+      </div>
     </div>
   )
 }
