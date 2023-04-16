1aad34dc4660f77ad2c092bd19c44065e5d934a5
Michael <michael_kaminski@outlook.com>
2023-04-16 16:05:02 -0500

    starting to work on chat

--- client/src/pages/Chat.tsx
+++ client/src/pages/Chat.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react'
 import Navbar from '../components/Navbar'
 import { fetchUserData } from '../axios/userRoutes'
+import Chatroom from '../components/Chatroom'
 
 const Chat = () => {
   return (
@@ -8,7 +9,16 @@ const Chat = () => {
       <div>
         <Navbar />
       </div>
-      Chat
+      <div className='mx-auto border bg-white w-[700px] h-[500px] mt-12 relative rounded-xl grid grid-cols-8 grid-rows-8'>
+        <div className='col-span-2 row-span-8 border'>
+          <h1 className='text-center text-xs font-bold mt-4'>
+            Current Chats (0)
+          </h1>
+        </div>
+        <div className='border col-span-6 row-span-8'>
+          <Chatroom />
+        </div>
+      </div>
     </div>
   )
 }
