585c2cf788c670ac17428dc6c48fc0b620930c50
Michael <michael_kaminski@outlook.com>
2023-04-19 15:16:06 -0500

    ''

--- client/src/static/icons/Add.tsx
+++ client/src/static/icons/Add.tsx
@@ -1,4 +1,6 @@
 import React from 'react'
 import AddCircleIcon from '@mui/icons-material/AddCircle'
+import PersonSearchIcon from '@mui/icons-material/PersonSearch'
 
 export const Add = () => <AddCircleIcon />
+export const AddPerson = () => <PersonSearchIcon />
