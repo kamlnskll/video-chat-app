1ae8a655727b1bd455b2e07f7c68a380de72de28
Michael <michael_kaminski@outlook.com>
2023-04-19 21:27:17 -0500

    added starting for setting page

--- client/src/pages/Settings.tsx
+++ client/src/pages/Settings.tsx
@@ -9,6 +9,14 @@ const Settings = () => {
       </div>
       <div className='bg-white border-gray-50 shadow-sm border rounded-lg w-[500px] h-[400px] mx-auto mt-28 pt-4'>
         <h1 className='text-center text-lg font-semibold'>Settings</h1>
+        <div className='text-center mt-4 rounded-lg text-sm'>
+          <h1 className='border w-1/3 mx-auto rounded-lg p-1 bg-gray-200 cursor-pointer'>
+            Edit Account
+          </h1>
+          <h1 className='border w-1/3 mx-auto mt-2 rounded-lg p-1 bg-gray-200 cursor-pointer'>
+            Preferences
+          </h1>
+        </div>
       </div>
     </div>
   )
