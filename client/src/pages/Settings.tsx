7ba69df8ba4f38e9ee50d28f7aa567388289c0a3
Michael <michael_kaminski@outlook.com>
2023-04-19 21:21:25 -0500

    getting started on settings page

--- client/src/pages/Settings.tsx
+++ client/src/pages/Settings.tsx
@@ -7,7 +7,9 @@ const Settings = () => {
       <div>
         <Navbar />
       </div>
-      Settings
+      <div className='bg-white border-gray-50 shadow-sm border rounded-lg w-[500px] h-[400px] mx-auto mt-28 pt-4'>
+        <h1 className='text-center text-lg font-semibold'>Settings</h1>
+      </div>
     </div>
   )
 }
